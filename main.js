bc2fe3cda9b3e8db0e2a473a14d0c9e55ca1f59d
Tusko Trush <tusko@photoinside.me>
2014-10-09 21:32:41 +0300

    changed RegEx added some php values

--- main.js
+++ main.js
@@ -28,9 +28,9 @@ define(function (require, exports, module) {
         this.lastFileName = "";
         this.cachedMatches = [];
         this.cachedWordList = [];
-        this.tokenDefinition = /[a-zA-Z][(_a-zA-Z0-9$,' )]{2,}/g;
+        this.tokenDefinition = /.{2,}/g;
         //this.currentTokenDefinition = /[a-zA-Z][a-zA-Z0-9_]+$/g;
-        this.currentTokenDefinition = /[a-zA-Z][a-zA-Z0-9_]+$/g;
+        this.currentTokenDefinition = /.{2,}/g;
     }
     
     
